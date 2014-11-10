8334c13f812d235fd71688a97dbd42445128e6b5
Alexander Handler <alexjhandler@gmail.com>
2014-11-10 16:11:59 -0500

    add lists and tasks. Lists and tasks related to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110204239) do
+ActiveRecord::Schema.define(version: 20141110210439) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -19,9 +19,21 @@ ActiveRecord::Schema.define(version: 20141110204239) do
     t.datetime "updated_at"
   end
 
+  create_table "lists", force: true do |t|
+    t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "memberships", force: true do |t|
     t.integer  "user_id"
     t.integer  "group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.integer  "list_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
