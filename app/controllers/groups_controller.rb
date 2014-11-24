f7f2c544def73cf8bc77ac15cfd955d88dced9b4
Alexander Handler <alexjhandler@gmail.com>
2014-11-24 10:34:05 -0500

    groups index and show view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,29 @@
 class GroupsController < ApplicationController 
-	def edit
+	before_action :set_group, only: [:show]
+   def edit
 		@invite = Invite.new
 	end
 
 	def create
    @invite = Invite.new(invite_params) # Make a new Invite
    @invite.sender_id = current_user.id # set the sender to the current user
-   if @invite.save
-      InviteMailer.new_user_invite(@invite, new_user_registration_path(:invite_token => @invite.token)).deliver #send the invite data to our mailer to deliver the email
-   else
+      if @invite.save
+         InviteMailer.new_user_invite(@invite, new_user_registration_path(:invite_token => @invite.token)).deliver #send the invite data to our mailer to deliver the email
+      else
       # oh no, creating an new invitation failed
+      end
    end
-end
+
+   def index
+      @usersgroups = current_user.groups
+   end
+   def show
+
+   end
+
+   private 
+   def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
